25e824707b423dd9b160c06c0639d8e9d2de3199
sknithin06 <nithinsk939@gmail.com>
2021-06-30 17:44:30 +0530

    UC2 is added

--- src/Employeewages.java
+++ src/Employeewages.java
@@ -8,6 +8,14 @@ public class Employeewages {
 		 	System.out.println("Employee is Present");
 		 else
 			System.out.println("Employee is Absent");
-	}
+		 
+		 double empRatePerHrs = 20;
+		 double empHrs = 8;
+		 double Salary=(empHrs * empRatePerHrs);
+		 if (empcheck == isPresent)
+			 System.out.println("Salary is ="+Salary);
+		 else
+			Salary = 0;
+		}
 }
 
