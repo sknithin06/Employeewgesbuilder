c6be804a9d0fc6468e547822a71f1a689c301d13
sknithin06 <nithinsk939@gmail.com>
2021-06-30 18:40:52 +0530

    UC3 is added

--- src/Employeewages.java
+++ src/Employeewages.java
@@ -16,6 +16,23 @@ public class Employeewages {
 			 System.out.println("Salary is ="+Salary);
 		 else
 			Salary = 0;
-		}
+		 
+		 int isFulltime = 1;
+		 int isParttime = 2;
+		 double empCheck = Math.floor(Math.random() * 10) % 3;
+		 double empRatePerHr = 20;
+		 if (empCheck == isFulltime) {
+			 double empHr = 12;
+			 double salary=(empHr * empRatePerHr);
+			 System.out.println("salary is ="+salary);
+		 } 
+		 else if (empCheck == isParttime) {
+			double emphr = 8;
+		    double salary=(emphr * empRatePerHr);
+		    System.out.println("salary is ="+salary);
+		 }
+		 else 
+			Salary = 0;
+	}
 }
 
