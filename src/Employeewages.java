8f709c126754d0d87900389f4ecd5444f3bfe0e3
sknithin06 <nithinsk939@gmail.com>
2021-06-30 17:05:53 +0530

    UC1 is added

--- src/Employeewages.java
+++ src/Employeewages.java
@@ -2,6 +2,12 @@
 public class Employeewages {
 	public static void main (String[] agrs) {
 		System.out.println("Welcome to Empwages");
+		 int isPresent = 1;
+		 double empcheck = Math.floor(Math.random() * 10) % 2;
+		 if (empcheck == isPresent)
+		 	System.out.println("Employee is Present");
+		 else
+			System.out.println("Employee is Absent");
 	}
 }
 
