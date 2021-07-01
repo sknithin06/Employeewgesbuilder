d58973a645523e333b3d1a8a6962c3b5507d1db5
sknithin06 <nithinsk939@gmail.com>
2021-07-01 12:13:09 +0530

    UC4 is added

--- src/Employeewages.java
+++ src/Employeewages.java
@@ -1,5 +1,9 @@
 
 public class Employeewages {
+	public static final int isFulltime = 1;
+	public static final int isParttime = 2;
+	public static final int empRatePerHrs = 20;
+	
 	public static void main (String[] agrs) {
 		System.out.println("Welcome to Empwages");
 		 int isPresent = 1;
@@ -9,7 +13,6 @@ public class Employeewages {
 		 else
 			System.out.println("Employee is Absent");
 		 
-		 double empRatePerHrs = 20;
 		 double empHrs = 8;
 		 double Salary=(empHrs * empRatePerHrs);
 		 if (empcheck == isPresent)
@@ -17,22 +20,34 @@ public class Employeewages {
 		 else
 			Salary = 0;
 		 
-		 int isFulltime = 1;
-		 int isParttime = 2;
 		 double empCheck = Math.floor(Math.random() * 10) % 3;
-		 double empRatePerHr = 20;
 		 if (empCheck == isFulltime) {
 			 double empHr = 12;
-			 double salary=(empHr * empRatePerHr);
+			 double salary=(empHr * empRatePerHrs);
 			 System.out.println("salary is ="+salary);
 		 } 
 		 else if (empCheck == isParttime) {
 			double emphr = 8;
-		    double salary=(emphr * empRatePerHr);
+		    double salary=(emphr * empRatePerHrs);
 		    System.out.println("salary is ="+salary);
 		 }
 		 else 
 			Salary = 0;
+		 
+		 int EmpCheck = (int) Math.floor(Math.random() * 10) % 3;
+		 int Emphrs = 0;
+		 int empWages = 0;
+		 switch ( EmpCheck) {
+		 	case  isFulltime :
+		 		  Emphrs = 8;
+		 		break ;
+		 	case  isParttime :
+		 		 Emphrs = 4;
+		 		break;
+		 	default:
+		 		 Emphrs = 0;
+		 }
+		 empWages = Emphrs * empRatePerHrs;
+		 System.out.println("empwage is:"+empWages);
 	}
 }
-
